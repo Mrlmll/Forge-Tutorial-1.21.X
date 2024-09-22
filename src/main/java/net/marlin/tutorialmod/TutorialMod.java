2e6f037d2c2701827619910711169a03800f09f7
Mrlmll <accounts@mrl-mll.de>
2024-09-22 19:39:42 +0200

    Added Items: Sapphire and Raw Sapphire. Also Added new Creative Tab

--- src/main/java/net/marlin/tutorialmod/TutorialMod.java
+++ src/main/java/net/marlin/tutorialmod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.marlin.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.marlin.tutorialmod.item.ModCreativeModTabs;
+import net.marlin.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -25,6 +28,10 @@ public class TutorialMod {
     public TutorialMod(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -38,7 +45,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
